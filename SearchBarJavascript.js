f9faaff826dcc2d341d1784626b0630cf01a361f
faraaz3348 <125925375+faraaz3348@users.noreply.github.com>
2023-03-03 12:05:32 -0500

    Update SearchBarJavascript.js
    Signed-off-by: faraaz3348 <125925375+faraaz3348@users.noreply.github.com>

--- SearchBarJavascript.js
+++ SearchBarJavascript.js
@@ -1,5 +1,14 @@
 
-
+API_KEY = '6aa86f5797de13c2b8d8eb2581248f51'
+user_query = 'Thor'
+fetch('https://api.themoviedb.org/3/search/movie?api_key='+API_KEY+'&language=en-US&query='+user_query+'&page=1&include_adult=false')
+		.then(res => res.json())
+    .then(data => {
+    	results = data.results;
+      for (let movie of results) {
+      	console.log(movie.original_title);
+      }
+    })
 
 
 
