6772090476922b64c6dbf285093564f9b7bac083
faraaz3348 <125925375+faraaz3348@users.noreply.github.com>
2023-03-06 14:40:20 -0500

    Delete api.js
    Signed-off-by: faraaz3348 <125925375+faraaz3348@users.noreply.github.com>

--- api.js
+++
@@ -1,5 +0,0 @@
-API_KEY = '6aa86f5797de13c2b8d8eb2581248f51'
-user_query = ''
-fetch('https://api.themoviedb.org/3/search/movie?api_key='+API_KEY+'&language=en-US&query='+user_query+'&page=1&include_adult=false')
-	.then(res => res.json())
-    .then(data => console.log(data))
